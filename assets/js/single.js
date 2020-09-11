2e10c311baca6b49084fefe1f507d6a336ab28d7
cat - lin <plummorgan@gmail.com>
2020-09-11 00:50:01 -0700

    added single issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,20 +1,34 @@
 //global variables
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function () {
+    var queryString = document.location.search;
+    // split the queryString
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+};
 
-
-
-
-var getRepoIssues = function(user, repo) {
-    var apiUrl = "https://api.github.com/repos/" + user + "/" + repo + "/issues?direction=asc";
+var getRepoIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(response) {
         //request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssue(data);
+                //check if api has paginated issuesa
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert("There was a problem with you request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -49,4 +63,16 @@ var displayIssue = function(issues) {
     }
 }
 
-getRepoIssues("facebook", "react");
+var displayWarning = function(repo) {
+    //add text to warn container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+
+getRepoName();
